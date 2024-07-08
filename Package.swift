0322295d13875584cee3e690be89f6abe807f3dc
Gábor Librecz <kuglee@gmail.com>
2024-07-08 23:26:01 +0200

    Fix iOS version in Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "swift-fullscreen-popup",
     platforms: [
-        .iOS(.v16),
+        .iOS(.v17),
         .macOS(.v13),
         .tvOS(.v16),
         .watchOS(.v9),
